e4eb85894256cdfcc16f1a818350eec1f0f3316f
luoyanlin <cnskyrin@163.com>
2024-12-26 22:04:42 +0800

    api config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
+/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
 })
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NODE_ENV === 'development'
+          ? 'http://localhost:3001/:path*'
+          : `${process.env.NEXT_PUBLIC_PROD_API_BASE_URL}/:path*` // 使用环境变量来配置生产环境 API 地址
+      }
+    ]
+  }
 }
 
 module.exports = withPWA(nextConfig) 
