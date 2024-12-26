5b5f5b22113d3b9584fcf2332c69bbbc51c65dd5
luoyanlin <cnskyrin@163.com>
2024-12-26 22:14:12 +0800

    Revert "api config"
    This reverts commit e4eb85894256cdfcc16f1a818350eec1f0f3316f.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
 })
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'development'
-          ? 'http://localhost:3001/:path*'
-          : `${process.env.NEXT_PUBLIC_PROD_API_BASE_URL}/:path*` // 使用环境变量来配置生产环境 API 地址
-      }
-    ]
-  }
+  reactStrictMode: true,
 }
 
 module.exports = withPWA(nextConfig) 
